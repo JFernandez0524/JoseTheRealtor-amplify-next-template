f7e5855d49f5f35d660723e0e2551be08df719ca
JFernandez0524 <njphonefix@gmail.com>
2025-12-02 09:56:15 -0500

    deleted process-leads api route and added s3 upload trigger functions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,8 +1,17 @@
-import { defineStorage } from '@aws-amplify/backend';
+import { defineStorage, defineFunction } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'leadFiles',
   access: (allow) => ({
     'leadFiles/*': [allow.entity('identity').to(['read', 'write', 'delete'])],
   }),
+
+  triggers: {
+    onUpload: defineFunction({
+      entry: './on-upload-handler.ts',
+    }),
+    onDelete: defineFunction({
+      entry: './on-delete-handler.ts',
+    }),
+  },
 });
