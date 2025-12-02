14c2a2c2a91662771b061eef6c05cce3e687e366
JFernandez0524 <njphonefix@gmail.com>
2025-12-02 09:43:24 -0500

    fixed dependency name issue

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,7 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'leadFiles',
+  access: (allow) => ({
+    'leadFiles/*': [allow.entity('identity').to(['read', 'write', 'delete'])],
+  }),
 });
