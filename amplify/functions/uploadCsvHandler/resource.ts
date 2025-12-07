52af499318c3489bc7ad9007d21af8c763835633
JFernandez0524 <njphonefix@gmail.com>
2025-12-06 21:10:00 -0500

    added google api secret to csvFunction definition

--- amplify/functions/uploadCsvHandler/resource.ts
+++ amplify/functions/uploadCsvHandler/resource.ts
@@ -10,6 +10,7 @@ export const uploadCsvHandler = defineFunction({
     BATCH_DATA_SERVER_TOKEN: secret('BATCH_DATA_SERVER_TOKEN'),
     KVCORE_API_KEY: secret('KVCORE_API_KEY'),
     GOHIGHLEVEL_API_KEY: secret('GOHIGHLEVEL_API_KEY'),
+    GOOGLE_MAPS_API_KEY: secret('GOOGLE_MAPS_API_KEY'),
     // NOTIFICATION_WEBHOOK_URL: process.env.NOTIFICATION_WEBHOOK_URL!,
     // AUDIT_LOG_BUCKET: process.env.AUDIT_LOG_BUCKET!,
   },
