4a17304e69d9e01c4b38a4aef3e3a5791256317a
JFernandez0524 <njphonefix@gmail.com>
2025-12-09 14:22:35 -0500

    deleted test page

--- app/test-page/page.tsx
+++
@@ -1,8 +0,0 @@
-import { cookiesClient } from '../utils/aws/auth/amplifyServerUtils.server';
-
-export default async function page() {
-  const response = await cookiesClient.queries.testFunction({
-    message: 'Hello World',
-  });
-  return <div>{JSON.stringify(response)}</div>;
-}
