946a86b7463210609df3237efa1df7eae03ffd9b
JFernandez0524 <njphonefix@gmail.com>
2025-12-21 00:45:44 -0500

    updated addusertogroup function

--- amplify/data/add-user-to-group/resource.ts
+++ amplify/data/add-user-to-group/resource.ts
@@ -1,6 +1,7 @@
+// amplify/data/add-user-to-group/resource.ts
 import { defineFunction } from '@aws-amplify/backend';
 
 export const addUserToGroup = defineFunction({
-  name: 'add-user-to-group',
+  name: 'addUserToGroup',
   entry: './handler.ts',
 });
